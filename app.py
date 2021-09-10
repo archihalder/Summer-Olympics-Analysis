7d9b9b87dfe2f92df0f0fd516948e6dac59d8c04
archihalder <archihalder23026558@gmail.com>
2021-09-10 21:30:06 +0530

    Overall Nations Participated Graph

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import preprocessor
 import medals
+import overall_analysis
+import plotly.express as px
 
 df = pd.read_csv('Data/athlete_events.csv')
 reg_df = pd.read_csv('Data/noc_regions.csv')
@@ -43,3 +45,39 @@ if menu == 'Medal Tally':
         st.title(selected_country + "'s Performance in " + str(selected_year))
 
     st.table(medal_tally)
+
+if menu == 'Overall Analysis':
+    editions = df['Year'].unique().shape[0] - 1
+    cities = df['City'].unique().shape[0]
+    sports = df['Sport'].unique().shape[0]
+    events = df['Event'].unique().shape[0]
+    athletes = df['Name'].unique().shape[0]
+    nation = df['Country'].unique().shape[0]
+
+    st.title('Top Statistics')
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.header('Editions')
+        st.title(editions)
+    with col2:
+        st.header('Cities')
+        st.title(cities)
+    with col3:
+        st.header('Sports')
+        st.title(sports)
+
+    col4, col5, col6 = st.columns(3)
+    with col4:
+        st.header('Events')
+        st.title(events)
+    with col5:
+        st.header('Athletes')
+        st.title(athletes)
+    with col6:
+        st.header('Countries')
+        st.title(nation)
+
+nations_over_time = overall_analysis.participating_nations_over_time(df)
+fig = px.line(nations_over_time, x='Year', y='No. of Countries')
+st.title('Participating Nations over the years')
+st.plotly_chart(fig)
