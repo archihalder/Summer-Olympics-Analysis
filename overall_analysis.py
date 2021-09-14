39c1402085e5b3153e435690f696e4b20af667e0
Archi Halder <archihalder23026558@gmail.com>
2021-09-14 12:40:23 +0530

    created the list of most successful athletes

--- overall_analysis.py
+++ overall_analysis.py
@@ -1,7 +1,20 @@
-def participating_nations_over_time(df):
-    nations_over_time = df.drop_duplicates(['Year', 'Country'])
-    nations_over_time = nations_over_time['Year'].value_counts()
-    nations_over_time = nations_over_time.reset_index()
-    nations_over_time = nations_over_time.sort_values('index')
-    nations_over_time.rename(columns={'index': 'Year', 'Year': 'No. of Countries'}, inplace=True)
-    return nations_over_time
+def data_over_time(df, col):
+    data = df.drop_duplicates(['Year', col])
+    data = data['Year'].value_counts()
+    data = data.reset_index()
+    data = data.sort_values('index')
+    data.rename(columns={'index': 'Year', 'Year': col}, inplace=True)
+    return data
+
+
+def most_successful(df, sport):
+    temp = df.dropna(subset=['Medal'])
+    if sport != 'Overall':
+        temp = temp[temp['Sport'] == sport]
+    temp = temp['Name'].value_counts()
+    temp = temp.reset_index()
+    temp = temp.merge(df, left_on='index', right_on='Name', how='left')
+    temp = temp[['index', 'Name_x', 'Sport', 'Country']]
+    temp = temp.drop_duplicates('index')
+    temp.rename(columns={'index': 'Name', 'Name_x': 'Medals'}, inplace=True)
+    return temp.head(10)
